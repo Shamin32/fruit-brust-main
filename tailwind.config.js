ae80cad3b2fcab2384883bb67635a72bf039eba6
shamin32 <shaminbba03@gmail.com>
2023-07-27 18:31:39 +0600

    my fruit shop

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+//** @type {import('tailwindcss').Config}*/ 
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         "custom-bg": "url('/images/team.png')",
+//       },
+//       colors: {
+//         primary: "#F85559",
+//       },
+//     },
+//   },
+//   plugins: [require("daisyui")],
+// };
